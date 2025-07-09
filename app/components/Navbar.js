5a6cdfccd5855c8a859f99f18de8d3a8b3ca742c
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-07-09 15:37:35 +0530

    Nav bar added

---
+++ app/components/Navbar.js
@@ -0,0 +1,37 @@
+'use client'
+
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="/">MyApp</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/register">Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
