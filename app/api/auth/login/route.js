c4a8323b6818e8d5f716f94dc84362ec0f5bd695
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-07-11 16:29:06 +0530

    Add validation in login

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -7,26 +7,30 @@ import { cookies } from 'next/headers';
 export async function POST(request) {
   const body = await request.json();
   const { email,password } = body;
-
+  if(!email && !password){
+     return NextResponse.json({ success:false,message: 'Provide credentials' }, {status: 401});
+  }
   try{
     const [users] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
     const user = users[0];
 
-    if (!user) return NextResponse.json({ message: 'Invalid credentials' }, {status: 401});
+    if (!user) return NextResponse.json({ success:false,message: 'Invalid credentials' }, {status: 401});
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return NextResponse.json({ message: 'Invalid credentials' }, {status: 401});
+    if (!match) return NextResponse.json({ success:false,message: 'Invalid credentials' }, {status: 401});
     const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
     
      cookies().set('token', token, {
       httpOnly: true,
       path: '/',
       maxAge: 60 * 60 * 24, // 1 day
+      secure: true,           // Only sent over HTTPS
+      sameSite: 'Lax',        // Helps prevent CSRF
     });
-    return NextResponse.json({ message: 'Login successful', token }, {status: 200});
+    return NextResponse.json({ success:true,message: 'Login successful', token }, {status: 200});
     
 
   }catch(error){
-    return NextResponse.json({ message: "User Login Failed" ,error:error}, {status: 400});
+    return NextResponse.json({ success:false,message: "User Login Failed" ,error:error}, {status: 400});
   }
 
 }    
