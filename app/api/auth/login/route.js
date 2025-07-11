7b4e101a6c155d9ed446e89b5cd52c952058bccf
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-07-11 15:51:14 +0530

    Send cookie when login success

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -2,6 +2,7 @@ import { db } from '@/lib/db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers';
 
 export async function POST(request) {
   const body = await request.json();
@@ -13,9 +14,16 @@ export async function POST(request) {
 
     if (!user) return NextResponse.json({ message: 'Invalid credentials' }, {status: 401});
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return NextResponse({ error: 'Invalid credentials' }, {status: 401});
+    if (!match) return NextResponse.json({ message: 'Invalid credentials' }, {status: 401});
     const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+     cookies().set('token', token, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 60 * 60 * 24, // 1 day
+    });
     return NextResponse.json({ message: 'Login successful', token }, {status: 200});
+    
 
   }catch(error){
     return NextResponse.json({ message: "User Login Failed" ,error:error}, {status: 400});
